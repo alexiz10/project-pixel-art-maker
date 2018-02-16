af7549725f6e9cb9ffced0cdde63b06b0213a2bb
Alexiz Hernandez <alexiz_hernandez@yahoo.com>
2018-02-16 14:54:51 -0700

    Added jQuery to project and got user color
    Added jQuery to index file and saved the selected color to a variable.

--- designs.js
+++ designs.js
@@ -1,5 +1,10 @@
-// Select color input
-// Select size input
+var color = '#000000';
+
+$('#colorPicker').on('input', function() {
+    color = $(this).val();
+});
+
+// Get column and row values
 
 // When size is submitted by the user, call makeGrid()
 
