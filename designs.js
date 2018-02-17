4d802262924829fde8b95d5361398d602b6a89e1
Alexiz Hernandez <alexiz_hernandez@yahoo.com>
2018-02-16 18:16:26 -0700

    Added code to makeGrid function
    Added code to makeGrid function to draw the grid on the screen depending on the user's input.

--- designs.js
+++ designs.js
@@ -13,5 +13,12 @@ $(':submit').on('click', function(e) {
 });
 
 function makeGrid(row, col) {
-
+    let table = $('#pixelCanvas');
+    table.empty();
+    for (let i = 1; i <= row; i++) {
+        table.append(`<tr id="row-${i}"></tr>`);
+        for (let j = 1; j <= col; j++) {
+            $(`#row-${i}`).append(`<td id="col-${i}-${j}"></td>`);
+        }
+    }
 }
